22a05b5c30e16b8aa44eeefcdd76c2d37625eded
Kishinsnkiy Oleg <oleg@kishinskiy.ru>
2018-12-09 22:10:21 +0300

    set blockchain

--- server/main.go
+++ server/main.go
@@ -1,25 +1,35 @@
 package main
 
 import (
-	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/protoc-gen-go/grpc"
+"golang.org/x/net/context"
+
+"github.com/Kishinskiy/grpc_blockchain/proto"
+	"github.com/Kishinskiy/grpc_blockchain/server/blockchain"
+	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
 func main(){
-	listener, err := net.Listen("tcp", "8080")
+	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalf("unable to listen on 8080 port: %w", err)
 	}
 
-	srv: := grpc.NewServer()
-	proto.RegisterBlockchainServer(srv, nil)
-	srv.Serve()
+	srv := grpc.NewServer()
+	proto.RegisterBlockchainServer(srv, &Server{})
+	srv.Serve(listener)
 }
 
-type Server {
+type Server struct {
+
 
 }
 
-func (s *Server) AddBlock)context.Context, *AddBlockRequest) ()
+func (s *Server) GetBlockchain(context.Context, *proto.GetBlockchainRequest) (*proto.GetBlockchainResponse, error) {
+	return new(proto.GetBlockchainResponse), nil
+}
+
+func (s *Server) AddBlock(context.Context, *proto.AddBlockRequest) (*proto.AddBlockResponse, error){
+  return new(proto.AddBlockResponse), nil
+}
